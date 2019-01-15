d49e762b4d5280296e2cf087341762d8ca70f729
akapan <akapan.r@hitachi-tstv.com>
2019-01-15 09:38:08 +0700

    Var Successed

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//การประกาศตัวแปรและค่าคงที่
+//Implicit Variable คือ การประกาศตัวแปรที่ เปลี่ยนแปลงค่าได้ และใช้ Data Type จากค่าเริ่มต้นมากำหนด ชนิดตัวแปร
+var nameString = "Akapan"
+
+nameString = "Doraemon"
+
+var number1 = 123
+
+number1 = 555
+
+//Explicit Variable
+var surName: String = "Rattanadilok Na Phuket"
+var number2: Int    = 555
+var number3: Double = 12.34
+var status: Bool    = true
+
+
