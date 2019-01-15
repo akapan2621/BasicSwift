c8b9c96633cdcf449132085c6adc84a3daebce9f
akapan <akapan.r@hitachi-tstv.com>
2019-01-15 09:48:54 +0700

    Test

--- VarAndConstant.playground/Contents.swift
+++ VarAndConstant.playground/Contents.swift
@@ -16,4 +16,12 @@ var number2: Int    = 555
 var number3: Double = 12.34
 var status: Bool    = true
 
+//Constant
+let name2: String = "Yoga"
+//name2 = "Run" ไม่สามารถ assign ค่าได้
+
+//Operation
+let answer1: Int = number1 + 100
+let number4 = "500"
+let answer2 = answer1 + Int(number3)// test
 
