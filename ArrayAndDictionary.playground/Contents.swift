189726a2514be28e5424427fadd3945ac2b38f07
akapan <akapan.r@hitachi-tstv.com>
2019-01-15 11:12:33 +0700

    Dictionary Successed

--- ArrayAndDictionary.playground/Contents.swift
+++ ArrayAndDictionary.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 var numbers = [11,22,33,44]
 var names   = ["AAA","BBB","CCC"]
-
+//About Array
 numbers[3]
 
 //การเพิ่มสมาชิกของ Array
@@ -19,3 +19,25 @@ print("Test Print")
 names
 //print("names[3] ==> " + names[3]) โบราณ
 print("name[3] ==> \(names[3])")
+
+let lenghtNumber: Int   = numbers.count
+
+print("lenghtNumber = \(lenghtNumber)")
+
+//About Dictionary
+var friend = ["key1":"Run","key2":"Walk","key3":"Jump","key4":"Swim"]
+print("friend ที่มีค่า key1 ==> \(friend["key1"]!)")
+
+let lenghtFriend = friend.count
+print("ขนาดของ Dictionary ==> \(lenghtFriend)")
+
+//Add member to Dictionary
+friend["ExtraFriend"] = "Jogging"
+print("friend ==> \(friend)")
+
+//Delete member of Dictionary
+friend.removeValue(forKey: "key1")
+print("friend ==> \(friend)")
+
+
+
